1911a9f42a34a21478a5b6349d415ab1b86e58b8
“R-winDev” <146721144+R-winDev@users.noreply.github.com>
2024-06-06 05:27:00 +0330

    resource-routing

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\IdeaController;
@@ -11,23 +10,11 @@ use Illuminate\Support\Facades\Route;
 // here we define the routes of our website pages.
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::group(['prefix' => 'ideas', 'as' => 'ideas.'],function()
-{
-    Route::post('/', [IdeaController::class, 'store'])->name('store');
+Route::resource('ideas',IdeaController::class)->except(['index','create','show'])->middleware('auth');
 
-    Route::get('/{idea}', [IdeaController::class, 'show'])->name('show');
+Route::resource('ideas',IdeaController::class)->only(['show']);
 
-    Route::group(['middleware' => ['auth']],function()
-    {
-        Route::get('/{idea}/edit', [IdeaController::class, 'edit'])->name('edit');
-
-        Route::put('/{idea}', [IdeaController::class, 'update'])->name('update');
-
-        Route::delete('/{idea}', [IdeaController::class, 'destroy'])->name('destroy');
-
-        Route::post('/{idea}/comments', [CommentController::class, 'store'])->name('comments.store');
-    });
-});
+Route::resource('ideas.comments',CommentController::class)->only(['store'])->middleware('auth');
 
 
 // Route::get('/profile', [ProfileController::class, 'index']);

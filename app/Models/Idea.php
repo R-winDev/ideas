e8bc1713a84178b9a0b3b2ed846acd4872be9549
“R-winDev” <146721144+R-winDev@users.noreply.github.com>
2024-06-07 20:41:12 +0330

    reduce-duplicated-queries

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -9,11 +9,7 @@ class Idea extends Model
 {
     use HasFactory;
 
-    // protected $guarded = [
-    //     'id',
-    //     'created_at',
-    //     'updated_at',
-    // ];
+    protected $with = ['user:id,name,image','comments.user:id,name,image'];
 
     protected $fillable = [
         'user_id',
